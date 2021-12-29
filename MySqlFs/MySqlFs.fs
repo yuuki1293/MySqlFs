4a28ed4710915a3e37dd78aeaa054be0b4eb6391
yuuki1293 <yuuki1293@outlook.jp>
2021-12-29 11:14:40 +0900

    とりあえず CREATE DATABASE 部分だけ作ってみた

--- MySqlFs/MySqlFs.fs
+++ MySqlFs/MySqlFs.fs
@@ -1,4 +1,51 @@
 ﻿namespace MySqlFs
 
-module Common=
+open System.Text
+open MySql.Data.MySqlClient
+
+[<AutoOpen>]
+module Function =
+    type DataBase = DataBase of string
+    type DataBaseCreateOut = DataBaseCreateOut of string
     
+
+    type MySql =
+        static member open'(connectionString: string) = new MySqlConnection(connectionString)
+        
+        //CREATE DATABASE
+        static member create1 (DataBase database) (conn: MySqlConnection) =
+            $"CREATE DATABASE {database} " |> DataBaseCreateOut, conn
+
+        static member create2 (DataBase database) (ifNotExists: bool) (conn: MySqlConnection) =
+            let ifNotExistsV =
+                if ifNotExists then
+                    "IF NOT EXISTS"
+                else
+                    ""
+
+            $"CREATE DATABASE {ifNotExistsV} {database}"
+            |> DataBaseCreateOut,
+            conn
+
+        static member defaultCharacterSetS (character: string) (DataBaseCreateOut command, conn: MySqlConnection) =
+            $"{command} DEFAULT CHARACTER SET = {character}"
+            |> DataBaseCreateOut,
+            conn
+
+        static member defaultCharacterSetE (character: Encoding) (DataBaseCreateOut command, conn: MySqlConnection) =
+            $"{command} DEFAULT CHARACTER SET = {character.BodyName}"
+            |> DataBaseCreateOut,
+            conn
+
+        static member defaultCollate (collation: string) (DataBaseCreateOut command, conn: MySqlConnection) =
+            $"{command} DEFAULT COLLATE = {collation}"
+            |> DataBaseCreateOut,
+            conn
+
+        static member defaultEncryption (enable: bool) (DataBaseCreateOut command, conn: MySqlConnection) =
+            let ins = if enable then "Y" else "N"
+
+            $"{command} DEFAULT ENCRYPTION = '{ins}'"
+            |> DataBaseCreateOut,
+            conn
+        
---
+++ MySqlFs/MySqlOp.fs
@@ -0,0 +1,51 @@
+﻿namespace MySqlFs
+
+open MySql.Data.MySqlClient
+open System.Text
+
+[<AutoOpen>]
+module MySqlBuilder=
+    type MysqlBuilder() =
+        member _.Yield _=
+            ()
+            
+        member _.Run (DataBaseCreateOut commandS,conn:MySqlConnection)=
+            try
+                conn.Open()
+                use mutable command = conn.CreateCommand()
+                command.CommandText <- commandS
+                command.Connection <- conn
+                command.ExecuteNonQuery()
+                |> Ok
+            with
+            | x -> Error x
+            
+        [<CustomOperation("open'")>]
+        member _.Open(_,conn)=
+            MySql.open'(conn)
+        
+        [<CustomOperation("create")>]
+        member _.Create(v:MySqlConnection,database:DataBase)=
+            MySql.create1 database v
+        
+        [<CustomOperation("create")>]
+        member _.Create(v:MySqlConnection,database:DataBase,ifNotExists: bool)=
+            MySql.create2 database ifNotExists v 
+        
+        [<CustomOperation("charset")>]
+        member _.CharSet(v:DataBaseCreateOut * MySqlConnection,character:string)=
+            MySql.defaultCharacterSetS character v
+            
+        [<CustomOperation("charset")>]
+        member _.CharSet(v:DataBaseCreateOut * MySqlConnection,character:Encoding)=
+            MySql.defaultCharacterSetE character v
+            
+        [<CustomOperation("collate")>]
+        member _.Collate(v:DataBaseCreateOut * MySqlConnection,collation: string)=
+            MySql.defaultCollate collation v
+            
+        [<CustomOperation("encryption")>]
+        member _.Encryption(v:DataBaseCreateOut * MySqlConnection,enable: bool)=
+            MySql.defaultEncryption enable v
+        
+    let mysql = MysqlBuilder()
