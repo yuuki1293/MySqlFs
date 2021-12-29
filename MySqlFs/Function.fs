fd683063cf912143791eafc4b685a71f0d548890
yuuki1293 <yuuki1293@outlook.jp>
2021-12-29 23:32:57 +0900

    CREATE TABLE を実装成功

--- MySqlFs/Function.fs
+++ MySqlFs/Function.fs
@@ -45,7 +45,9 @@ module Function =
     type Common =
         static member open'(connectionString: string) = new MySqlConnection(connectionString)
 
-        static member runExecuteNonQuery(commandS, conn: MySqlConnection) =
+        static member runExecuteNonQuery(commandS: string, connS: string) =
+            let conn = Common.open' connS
+
             try
                 conn.Open()
                 use mutable command = conn.CreateCommand()
@@ -56,115 +58,100 @@ module Function =
             | x -> Error x
 
     type Original =
-        static member createDatabase1 (DataBase database) (conn: MySqlConnection) =
-            $"CREATE DATABASE {database}" |> DataBaseCreateOut, conn
+        static member createDatabase1 (DataBase database) =
+            $"CREATE DATABASE {database}" |> DataBaseCreateOut
 
-        static member createDatabase2 (DataBase database) (_: IfNotExists) (conn: MySqlConnection) =
+        static member createDatabase2 (DataBase database) (_: IfNotExists) =
             $"CREATE DATABASE IF NOT EXISTS {database}"
-            |> DataBaseCreateOut,
-            conn
+            |> DataBaseCreateOut
 
-        static member createTable1 (Table table) (conn: MySqlConnection) =
-            $"CREATE TABLE {table}" |> TableCreateOutWithCols, conn
+        static member createTable1 (Table table) =
+            $"CREATE TABLE {table}" |> TableCreateOutWithCols
 
-        static member createTable2 (Table table) (_: IfNotExists) (conn: MySqlConnection) =
+        static member createTable2 (Table table) (_: IfNotExists) =
             $"CREATE TABLE IF NOT EXISTS {table}"
-            |> TableCreateOutWithCols,
-            conn
+            |> TableCreateOutWithCols
 
-        static member createTable3 (Table table) (_: Temporary) (conn: MySqlConnection) =
+        static member createTable3 (Table table) (_: Temporary) =
             $"CREATE TEMPORARY TABLE {table}"
-            |> TableCreateOutWithCols,
-            conn
+            |> TableCreateOutWithCols
 
-        static member createTable4 (Table table) (_: IfNotExists) (_: Temporary) (conn: MySqlConnection) =
+        static member createTable4 (Table table) (_: IfNotExists) (_: Temporary)=
             $"CREATE TEMPORARY TABLE IF NOT EXISTS {table}"
-            |> TableCreateOutWithCols,
-            conn
+            |> TableCreateOutWithCols
 
-        static member drop1 (DataBase database) (conn: MySqlConnection) =
-            $"DROP DATABASE {database} " |> DataBaseDropOut, conn
+        static member drop1 (DataBase database)=
+            $"DROP DATABASE {database} " |> DataBaseDropOut
 
-        static member drop2 (DataBase database) (_: IfExists) (conn: MySqlConnection) =
+        static member drop2 (DataBase database) (_: IfExists) =
             $"DROP DATABASE IF EXISTS {database}"
-            |> DataBaseDropOut,
-            conn
+            |> DataBaseDropOut
 
-        static member alter (DataBase database) (conn: MySqlConnection) =
-            $"ALTER DATABASE {database}" |> DataBaseAlterOut, conn
+        static member alter (DataBase database)=
+            $"ALTER DATABASE {database}" |> DataBaseAlterOut
 
     type CharSet =
-        static member createDatabase (character: string) (DataBaseCreateOut command, conn: MySqlConnection) =
+        static member createDatabase (character: string) (DataBaseCreateOut command) =
             $"{command} DEFAULT CHARACTER SET = {character}"
-            |> DataBaseCreateOut,
-            conn
+            |> DataBaseCreateOut
 
-        static member alterDatabase (character: string) (DataBaseAlterOut command, conn: MySqlConnection) =
+        static member alterDatabase (character: string) (DataBaseAlterOut command) =
             $"{command} DEFAULT CHARACTER SET = {character}"
-            |> DataBaseAlterOut,
-            conn
+            |> DataBaseAlterOut
 
-        static member createTable (character: string) (command: INextTableCreateOut, conn: MySqlConnection) =
+        static member createTable (character: string) (command: INextTableCreateOut) =
             $"{command.value} DEFAULT CHARACTER SET = {character}"
-            |> TableCreateOut,
-            conn
+            |> TableCreateOut
 
     type Collate =
-        static member createDatabase (collation: string) (DataBaseCreateOut command, conn: MySqlConnection) =
+        static member createDatabase (collation: string) (DataBaseCreateOut command) =
             $"{command} DEFAULT COLLATE = {collation}"
-            |> DataBaseCreateOut,
-            conn
+            |> DataBaseCreateOut
 
-        static member alterDatabase (collation: string) (DataBaseAlterOut command, conn: MySqlConnection) =
+        static member alterDatabase (collation: string) (DataBaseAlterOut command) =
             $"{command} DEFAULT COLLATE = {collation}"
-            |> DataBaseAlterOut,
-            conn
+            |> DataBaseAlterOut
 
-        static member createTable (collation: string) (command: INextTableCreateOut, conn: MySqlConnection) =
+        static member createTable (collation: string) (command: INextTableCreateOut) =
             $"{command} DEFAULT COLLATE = {collation}"
-            |> TableCreateOut,
-            conn
+            |> TableCreateOut
 
     type Encryption =
-        static member createDatabase (enable: bool) (DataBaseCreateOut command, conn: MySqlConnection) =
+        static member createDatabase (enable: bool) (DataBaseCreateOut command) =
             let ins = if enable then "Y" else "N"
 
             $"{command} DEFAULT ENCRYPTION = '{ins}'"
-            |> DataBaseCreateOut,
-            conn
+            |> DataBaseCreateOut
 
-        static member alterDatabase (enable: bool) (DataBaseAlterOut command, conn: MySqlConnection) =
+        static member alterDatabase (enable: bool) (DataBaseAlterOut command) =
             let ins = if enable then "Y" else "N"
 
             $"{command} DEFAULT ENCRYPTION = '{ins}'"
-            |> DataBaseAlterOut,
-            conn
+            |> DataBaseAlterOut
 
     type Cols =
-        static member createTable (TableCol tablecol) (command: INextCols, conn: MySqlConnection) =
-            $"{command.value} {tablecol}" |> TableCreateOut, conn
+        static member createTable (TableCol tablecol) (command: INextCols) =
+            $"{command.value} {tablecol}" |> TableCreateOut
 
     type Engine =
-        static member createTable (engineName: string) (command: INextTableCreateOut, conn: MySqlConnection) =
+        static member createTable (engineName: string) (command: INextTableCreateOut) =
             $"{command.value} ENGINE = {engineName}"
-            |> TableCreateOut,
-            conn
+            |> TableCreateOut
 
     type Comment =
-        static member createTable (comment: string) (command: INextTableCreateOut, conn: MySqlConnection) =
-            $"{command.value} COMMENT = {comment}"
-            |> TableCreateOut,
-            conn
+        static member createTable (comment: string) (command: INextTableCreateOut) =
+            $"{command.value} COMMENT = '{comment}'"
+            |> TableCreateOut
 
     type Run =
-        static member createDatabase(DataBaseCreateOut command, conn: MySqlConnection) =
+        static member createDatabase(DataBaseCreateOut command, conn: string) =
             Common.runExecuteNonQuery (command, conn)
 
-        static member dropDatabase(DataBaseDropOut command, conn: MySqlConnection) =
+        static member dropDatabase(DataBaseDropOut command, conn: string) =
             Common.runExecuteNonQuery (command, conn)
 
-        static member alterDatabase(DataBaseAlterOut command, conn: MySqlConnection) =
+        static member alterDatabase(DataBaseAlterOut command, conn: string) =
             Common.runExecuteNonQuery (command, conn)
 
-        static member createTable(command: INextTableCreateOut, conn: MySqlConnection) =
+        static member createTable(command: INextTableCreateOut, conn: string) =
             Common.runExecuteNonQuery (command.value, conn)
--- MySqlFs/MySqlOp.fs
+++ MySqlFs/MySqlOp.fs
@@ -8,86 +8,87 @@ open MySqlFs.Function
 module MySqlBuilder =
     type MySqlBuilder(conn: string) =
 
-        member _.Yield _ = Common.open' conn
+        member _.Yield _ = ()
 
         //CREATE DATABASE
-        member _.Run(v: DataBaseCreateOut * MySqlConnection) = Run.createDatabase v
+        member _.Run(v: DataBaseCreateOut) =
+            Run.createDatabase (v, conn)
 
         [<CustomOperation("create")>]
-        member _.Create(v: MySqlConnection, database: DataBase) = Original.createDatabase1 database v
+        member _.Create(_:unit, database: DataBase) = Original.createDatabase1 database
 
         [<CustomOperation("create")>]
-        member _.Create(v: MySqlConnection, database: DataBase, ifNotExists: IfNotExists) =
-            Original.createDatabase2 database ifNotExists v
+        member _.Create(_:unit, database: DataBase, ifNotExists: IfNotExists) =
+            Original.createDatabase2 database ifNotExists
 
         [<CustomOperation("charset")>]
-        member _.CharSet(v: DataBaseCreateOut * MySqlConnection, character: string) = CharSet.createDatabase character v
+        member _.CharSet(v: DataBaseCreateOut, character: string) = CharSet.createDatabase character v
 
         [<CustomOperation("collate")>]
-        member _.Collate(v: DataBaseCreateOut * MySqlConnection, collation: string) = Collate.createDatabase collation v
+        member _.Collate(v: DataBaseCreateOut, collation: string) = Collate.createDatabase collation v
 
         [<CustomOperation("encryption")>]
-        member _.Encryption(v: DataBaseCreateOut * MySqlConnection, enable: bool) = Encryption.createDatabase enable v
+        member _.Encryption(v: DataBaseCreateOut, enable: bool) = Encryption.createDatabase enable v
 
         //DROP DATABASE
-        member _.Run(v: DataBaseDropOut * MySqlConnection) = Run.dropDatabase v
+        member _.Run(v: DataBaseDropOut) = Run.dropDatabase (v, conn)
 
         [<CustomOperation("drop")>]
-        member _.Drop(v: MySqlConnection, database: DataBase) = Original.drop1 database v
+        member _.Drop(_:unit, database: DataBase) = Original.drop1 database
 
         [<CustomOperation("drop")>]
-        member _.Drop(v: MySqlConnection, database: DataBase, ifExists: IfExists) = Original.drop2 database ifExists v
+        member _.Drop(_:unit, database: DataBase, ifExists: IfExists) = Original.drop2 database ifExists
 
         //ALTER DATABASE
-        member _.Run(v: DataBaseAlterOut * MySqlConnection) = Run.alterDatabase v
+        member _.Run(v: DataBaseAlterOut) = Run.alterDatabase (v,conn)
 
         [<CustomOperation("alter")>]
-        member _.Alter(v: MySqlConnection, database: DataBase) = Original.alter database v
+        member _.Alter(_:unit, database: DataBase) = Original.alter database
 
         [<CustomOperation("charset")>]
-        member _.CharSet(v: DataBaseAlterOut * MySqlConnection, character: string) = CharSet.alterDatabase character v
+        member _.CharSet(v: DataBaseAlterOut, character: string) = CharSet.alterDatabase character v
 
         [<CustomOperation("collate")>]
-        member _.Collate(v: DataBaseAlterOut * MySqlConnection, collation: string) = Collate.alterDatabase collation v
+        member _.Collate(v: DataBaseAlterOut, collation: string) = Collate.alterDatabase collation v
 
         [<CustomOperation("encryption")>]
-        member _.Encryption(v: DataBaseAlterOut * MySqlConnection, enable: bool) = Encryption.alterDatabase enable v
+        member _.Encryption(v: DataBaseAlterOut, enable: bool) = Encryption.alterDatabase enable v
 
         //CREATE TABLE
-        member _.Run(v: INextTableCreateOut * MySqlConnection) = Run.createTable v
+        member _.Run(v: INextTableCreateOut) = Run.createTable (v, conn)
 
         [<CustomOperation("create")>]
-        member _.Create(v: MySqlConnection, table: Table) = Original.createTable1 table v
+        member _.Create(_:unit, table: Table) = Original.createTable1 table
 
         [<CustomOperation("create")>]
-        member _.Create(v: MySqlConnection, table: Table, ifNotExists: IfNotExists) =
-            Original.createTable2 table ifNotExists v
+        member _.Create(_:unit, table: Table, ifNotExists: IfNotExists) =
+            Original.createTable2 table ifNotExists
 
         [<CustomOperation("create")>]
-        member _.Create(v: MySqlConnection, table: Table, temporary: Temporary) =
-            Original.createTable3 table temporary v
+        member _.Create(_:unit, table: Table, temporary: Temporary) =
+            Original.createTable3 table temporary
 
         [<CustomOperation("create")>]
-        member _.Create(v: MySqlConnection, table: Table, ifNotExists: IfNotExists, temporary: Temporary) =
-            Original.createTable4 table ifNotExists temporary v
+        member _.Create(_:unit, table: Table, ifNotExists: IfNotExists, temporary: Temporary) =
+            Original.createTable4 table ifNotExists temporary 
 
         [<CustomOperation("create")>]
-        member _.Create(v: MySqlConnection, table: Table, temporary: Temporary, ifNotExists: IfNotExists) =
-            Original.createTable4 table ifNotExists temporary v
+        member _.Create(_:unit, table: Table, temporary: Temporary, ifNotExists: IfNotExists) =
+            Original.createTable4 table ifNotExists temporary 
 
         [<CustomOperation("cols")>]
-        member _.Cols(v: INextCols * MySqlConnection, tablecol: TableCol) = Cols.createTable tablecol v
+        member _.Cols(v: INextCols, tablecol: TableCol) = Cols.createTable tablecol v
 
         [<CustomOperation("charset")>]
-        member _.CharSet(v: INextTableCreateOut * MySqlConnection, character: string) = CharSet.createTable character v
+        member _.CharSet(v: INextTableCreateOut, character: string) = CharSet.createTable character v
 
         [<CustomOperation("collate")>]
-        member _.Collate(v: INextTableCreateOut * MySqlConnection, collation: string) = Collate.createTable collation v
+        member _.Collate(v: INextTableCreateOut, collation: string) = Collate.createTable collation v
 
         [<CustomOperation("engine")>]
-        member _.Engine(v: INextTableCreateOut * MySqlConnection, engineName: string) = Engine.createTable engineName v
+        member _.Engine(v: INextTableCreateOut, engineName: string) = Engine.createTable engineName v
 
         [<CustomOperation("comment")>]
-        member _.Comment(v: INextTableCreateOut * MySqlConnection, comment: string) = Comment.createTable comment v
+        member _.Comment(v: INextTableCreateOut, comment: string) = Comment.createTable comment v
 
     let mysql connection = MySqlBuilder connection
