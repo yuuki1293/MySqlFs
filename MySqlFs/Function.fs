546d72ca0974eaaa907a389662c6c57b75ca0d7e
yuuki1293 <yuuki1293@outlook.jp>
2021-12-29 17:53:01 +0900

    フォーマットをかけた

--- MySqlFs/Function.fs
+++ MySqlFs/Function.fs
@@ -1,4 +1,5 @@
 ﻿namespace MySqlFs
+
 open MySql.Data.MySqlClient
 
 [<AutoOpen>]
@@ -11,10 +12,10 @@ module Function =
     type DataBaseCreateOut = DataBaseCreateOut of string
     type DataBaseDropOut = DataBaseDropOut of string
     type DataBaseAlterOut = DataBaseAlterOut of string
-    
-    type Common=
+
+    type Common =
         static member open'(connectionString: string) = new MySqlConnection(connectionString)
-        
+
         static member runExecuteNonQuery(commandS, conn: MySqlConnection) =
             try
                 conn.Open()
@@ -24,18 +25,18 @@ module Function =
                 command.ExecuteNonQuery() |> Ok
             with
             | x -> Error x
-            
-    type Original=
+
+    type Original =
         static member create1 (DataBase database) (conn: MySqlConnection) =
             $"CREATE DATABASE {database} "
             |> DataBaseCreateOut,
             conn
-        
+
         static member create2 (DataBase database) (_: IfNotExists) (conn: MySqlConnection) =
             $"CREATE DATABASE IF NOT EXISTS {database}"
             |> DataBaseCreateOut,
             conn
-        
+
         static member drop1 (DataBase database) (conn: MySqlConnection) =
             $"DROP DATABASE {database} " |> DataBaseDropOut, conn
 
@@ -43,53 +44,53 @@ module Function =
             $"DROP DATABASE IF EXISTS {database}"
             |> DataBaseDropOut,
             conn
-        
-        static member alter (DataBase database) (conn: MySqlConnection) = $"ALTER DATABASE {database}"|>DataBaseAlterOut, conn
 
-    type CharSet=
+        static member alter (DataBase database) (conn: MySqlConnection) =
+            $"ALTER DATABASE {database}" |> DataBaseAlterOut, conn
+
+    type CharSet =
         static member createDatabase (character: string) (DataBaseCreateOut command, conn: MySqlConnection) =
             $"{command} DEFAULT CHARACTER SET = {character}"
             |> DataBaseCreateOut,
             conn
-        
+
         static member alterDatabase (character: string) (DataBaseAlterOut command, conn: MySqlConnection) =
             $"{command} DEFAULT CHARACTER SET = {character}"
             |> DataBaseAlterOut,
             conn
-            
-    type Collate=
+
+    type Collate =
         static member createDatabase (collation: string) (DataBaseCreateOut command, conn: MySqlConnection) =
             $"{command} DEFAULT COLLATE = {collation}"
             |> DataBaseCreateOut,
             conn
-        
+
         static member alterDatabase (collation: string) (DataBaseAlterOut command, conn: MySqlConnection) =
             $"{command} DEFAULT COLLATE = {collation}"
             |> DataBaseAlterOut,
             conn
-            
-    type Encryption=
+
+    type Encryption =
         static member createDatabase (enable: bool) (DataBaseCreateOut command, conn: MySqlConnection) =
             let ins = if enable then "Y" else "N"
 
             $"{command} DEFAULT ENCRYPTION = '{ins}'"
             |> DataBaseCreateOut,
             conn
-            
+
         static member alterDatabase (enable: bool) (DataBaseAlterOut command, conn: MySqlConnection) =
             let ins = if enable then "Y" else "N"
 
             $"{command} DEFAULT ENCRYPTION = '{ins}'"
             |> DataBaseAlterOut,
             conn
-            
-    type Run=
-        static member createDatabase(DataBaseCreateOut command, conn: MySqlConnection)=
-            Common.runExecuteNonQuery(command,conn)
-            
-        static member dropDatabase(DataBaseDropOut command, conn: MySqlConnection)=
-            Common.runExecuteNonQuery(command,conn)
-        
-        static member runAlterDatabase(DataBaseAlterOut command, conn: MySqlConnection)=
-            Common.runExecuteNonQuery(command,conn)
-    
+
+    type Run =
+        static member createDatabase(DataBaseCreateOut command, conn: MySqlConnection) =
+            Common.runExecuteNonQuery (command, conn)
+
+        static member dropDatabase(DataBaseDropOut command, conn: MySqlConnection) =
+            Common.runExecuteNonQuery (command, conn)
+
+        static member runAlterDatabase(DataBaseAlterOut command, conn: MySqlConnection) =
+            Common.runExecuteNonQuery (command, conn)
--- MySqlFs/MySqlOp.fs
+++ MySqlFs/MySqlOp.fs
@@ -12,10 +12,9 @@ module MySqlBuilder =
 
         [<CustomOperation("open'")>]
         member _.Open(_, conn) = Common.open' conn
-        
+
         //CREATE DATABASE
-        member _.Run(v: DataBaseCreateOut * MySqlConnection) =
-            Run.createDatabase v
+        member _.Run(v: DataBaseCreateOut * MySqlConnection) = Run.createDatabase v
 
         [<CustomOperation("create")>]
         member _.Create(v: MySqlConnection, database: DataBase) = Original.create1 database v
@@ -25,41 +24,36 @@ module MySqlBuilder =
             Original.create2 database ifNotExists v
 
         [<CustomOperation("charset")>]
-        member _.CharSet(v: DataBaseCreateOut * MySqlConnection, character: string) =
-            CharSet.createDatabase character v
+        member _.CharSet(v: DataBaseCreateOut * MySqlConnection, character: string) = CharSet.createDatabase character v
 
         [<CustomOperation("collate")>]
         member _.Collate(v: DataBaseCreateOut * MySqlConnection, collation: string) = Collate.createDatabase collation v
 
         [<CustomOperation("encryption")>]
         member _.Encryption(v: DataBaseCreateOut * MySqlConnection, enable: bool) = Encryption.createDatabase enable v
-        
+
         //DROP DATABASE
-        member _.Run(v: DataBaseDropOut * MySqlConnection) =
-            Run.dropDatabase(v)
-         
+        member _.Run(v: DataBaseDropOut * MySqlConnection) = Run.dropDatabase (v)
+
         [<CustomOperation("drop")>]
-        member _.Drop(v: MySqlConnection, database: DataBase)=
-            Original.drop1 database v
-        
+        member _.Drop(v: MySqlConnection, database: DataBase) = Original.drop1 database v
+
         [<CustomOperation("drop")>]
-        member _.Drop(v: MySqlConnection, database: DataBase, ifExists:IfExists)=
-            Original.drop2 database ifExists v
-        
+        member _.Drop(v: MySqlConnection, database: DataBase, ifExists: IfExists) = Original.drop2 database ifExists v
+
         //ALTER DATABASE
-        member _.Run(v: DataBaseAlterOut * MySqlConnection) =
-            Run.runAlterDatabase(v)
+        member _.Run(v: DataBaseAlterOut * MySqlConnection) = Run.runAlterDatabase (v)
 
         [<CustomOperation("alter")>]
         member _.Alter(v: MySqlConnection, database: DataBase) = Original.alter database v
 
         [<CustomOperation("charset")>]
-        member _.CharSet(v: DataBaseAlterOut * MySqlConnection, character: string) =
-            CharSet.alterDatabase character v
+        member _.CharSet(v: DataBaseAlterOut * MySqlConnection, character: string) = CharSet.alterDatabase character v
 
         [<CustomOperation("collate")>]
         member _.Collate(v: DataBaseAlterOut * MySqlConnection, collation: string) = Collate.alterDatabase collation v
 
         [<CustomOperation("encryption")>]
         member _.Encryption(v: DataBaseAlterOut * MySqlConnection, enable: bool) = Encryption.alterDatabase enable v
+
     let mysql = MySqlBuilder()
